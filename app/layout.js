33ac8bfeb2c4613ff7750efb4db9833e0bf52d03
Luke Song <hyeonminsong7@gmail.com>
2023-11-09 16:44:58 -0800

    ADDED:FAVICONS

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+        <Head>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body className={inter.className}>{children}</body>
     </html>
   );
 }
